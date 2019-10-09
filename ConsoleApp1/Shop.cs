dc68d577b4378658923f09c0a7c5e37d1e9c480f
zionmatthews <54289230+zionmatthews@users.noreply.github.com>
2019-10-09 14:51:11 -0500

    Update Shop.cs

--- ConsoleApp1/Shop.cs
+++ ConsoleApp1/Shop.cs
@@ -9,12 +9,15 @@ namespace Assessment1
     class Shop
     {
 
-        
+        string choice = "";
 
 
         static protected Item[] PlayerInventory;
         static protected Item[] StoreInventory;
         static protected Item[] ManagerInventory;
+        static protected int PlayerFuns;
+        static protected int StoreFuns;
+        
 
         Inventory inventory = new Inventory();
         ShopInventory shopInventory = new ShopInventory();
@@ -26,7 +29,19 @@ namespace Assessment1
             StoreInventory = shopInventory.Get_itemList;
             PlayerInventory = inventory.Get_playerList;
             ManagerInventory = manager.GetmanagerInv;
+            PlayerFuns = 500;
+            StoreFuns = 1000;
+            
+        }
 
+        public int GetFuns()
+        {
+            return PlayerFuns;
+        }
+
+        public int GetStoreFuns()
+        {
+            return StoreFuns;
         }
 
 
@@ -39,7 +54,7 @@ namespace Assessment1
 
             string choice = "";
 
-            while (choice != "5")
+            while (choice != "6")
             {
                 //Store Menu
                 Console.WriteLine("This is my little shop of terror!");
@@ -59,7 +74,7 @@ namespace Assessment1
 
                 if (choice == "1")
                 {
-                    Inventory();
+                    CheckInventory();
                 }
 
                 else if (choice == "2")
@@ -72,7 +87,7 @@ namespace Assessment1
                     BuyItems();
                 }
 
-                else if (choice == "Lanch_EXEC")
+                else if (choice == "EXEC_DIR")
                 {
                     SecretMenu();
                 }
@@ -84,30 +99,128 @@ namespace Assessment1
 
         }
 
-        static void Inventory()
+        public void CheckInventory()
         {
             //Checks the player Inventory
-            Console.WriteLine("\n Here is your inventory.");
-            for (int i = 0; i < PlayerInventory.Length; i++)
+            Console.WriteLine("\nHere is your inventory");
+            Console.WriteLine("");
+            for(int i = 0; i < PlayerInventory.Length; i++)
             {
                 Console.WriteLine((i + 1) + ": " + PlayerInventory[i].GetName());
             }
+            Console.WriteLine("Zbucks: " + PlayerFuns);
+
         }
 
-        static void SellItems()
+        public void SellItems()
         {
             //Sells any item in the Player Inventory
+            string choice = "";
+            int zbucks = GetFuns();
+            int Store = GetStoreFuns();
+
+            while (choice != "0")
+            {
+                Console.WriteLine("What are you selling today");
+                Console.WriteLine("0: Exit");
+
+                for(int i = 0; i < PlayerInventory.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + ": " + PlayerInventory[i].GetName() +" " + PlayerInventory[i].GetCost() + " Zbucks");
+                }
+                choice = Console.ReadLine();
+
+                for(int i = 0; i < PlayerInventory.Length; i++)
+                {
+                    if(Convert.ToInt32(choice) == (i + 1))
+                    {
+                        Console.WriteLine("You sold " + PlayerInventory[i].GetName() + " " + PlayerInventory[i].GetCost() + " Zbuckz");
+                        shopInventory.Add(StoreInventory, PlayerInventory[i]);
+                        StoreInventory = shopInventory.Get_itemList;
+                        PlayerFuns = (zbucks + PlayerInventory[i].GetCost());
+                        StoreFuns = (Store - PlayerInventory[i].GetCost());
+                        inventory.Remove(PlayerInventory, i);
+                        PlayerInventory = inventory.Get_playerList;
+
+                        choice = "0";
+                    }
+                }
+
+            } 
         }
 
-        static void BuyItems()
+        public void BuyItems()
         {
             //Buys items in the shop inventory
+            string choice = "";
+            int zbucks = GetFuns();
+            int Store = GetStoreFuns();
+
+            while(choice != "0")
+            {
+                Console.WriteLine("What are you buying today?");
+                Console.WriteLine("0: Exit");
+
+                for(int i = 0; i < StoreInventory.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + ": " + StoreInventory[i].GetName() + " " + StoreInventory[i].GetCost() + " Zbucks");
+                }
+                choice = Console.ReadLine();
+
+                for(int i = 0; i < StoreInventory.Length; i++)
+                {
+                    if (Convert.ToInt32(choice) == (i + 1))
+                    {
+                        Console.WriteLine("You bought " + StoreInventory[i].GetName() + " " + StoreInventory[i].GetCost() + " Zbucks");
+                        inventory.Add(PlayerInventory, StoreInventory[i]);
+                        PlayerInventory = inventory.Get_playerList;
+                        PlayerFuns = (zbucks - StoreInventory[i].GetCost());
+                        StoreFuns = (Store + StoreInventory[i].GetCost());
+                        shopInventory.Remove(StoreInventory, i);
+                        StoreInventory = shopInventory.Get_itemList;
+
+                        choice = "0";
+                    }
+                }
+            }
         }
 
-        static void SecretMenu()
+        public void SecretMenu()
         {
             //The secret menu activated by Lanch_EXEC
-            Console.WriteLine("You just unlocked the managers inventory!!");
+            string choice = "";
+
+            while(choice != "0")
+            {
+                Console.WriteLine("You just unlocked the managers inventory!!");
+                Console.WriteLine("What are you taking?");
+                Console.WriteLine("0: Exit");
+
+                for(int i = 0; i < ManagerInventory.Length; i++)
+                {
+                    Console.WriteLine((i + 1) + ": " + ManagerInventory[i].GetName());
+                }
+                choice = Console.ReadLine();
+
+                for(int i = 0; i < ManagerInventory.Length; i++)
+                {
+                    if(Convert.ToInt32(choice) == (i + 1))
+                    {
+                        Console.WriteLine("You Took " + ManagerInventory[i].GetName() + "from the manager");
+                        inventory.Add(PlayerInventory, ManagerInventory[i]);
+                        PlayerInventory = inventory.Get_playerList;
+                        manager.Remove(ManagerInventory, i);
+                        ManagerInventory = manager.GetmanagerInv;
+
+                        choice = "0";
+                    }
+                }
+            }
+
+
+
+
+            
         }
 
 
